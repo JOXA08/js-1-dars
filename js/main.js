48644471de0a59af6a2eea35ac41fcc5e3305f49
Jahongir Safarov <168184861+JOXA08@users.noreply.github.com>
2024-05-06 17:21:44 +0500

    js 1dars
    zeromax itcentr

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*console.log("Men faqat konsol oynasida korinaman");
+alert("saytimizga hush kelibsiz");
+document.write(0,4-3);*/
+
+/*let x = 5;
+let z = 7
+console.log(x + z)*/
+/*let x = 10, z = 7, y = 25
+console.log(x+z-y)*/
+
+/* Data types in JS *//*
+let soz = "JavaScript";
+let son = 5;
+document.write(son)*/
+
+/*Math inJS*/
+
+let x = 0;
+/*x += 5;*/
+/*x -= 1;*/
+/*x *= 5;*/
+/*x /= 2;*/
+/*let qoldiq = x % 7;*/
+/*let natija = x**2*/
+/*++x;*/
+/*x--;*/
+/*x++;
+x++;
+x++;*/
+
+
+document.write(x)
+
+
